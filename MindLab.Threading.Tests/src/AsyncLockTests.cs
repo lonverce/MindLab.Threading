bed434d4e274eece24c133e52eaf07053d740294
lilonghua <lilonghua@genew.com>
2020-03-16 19:56:19 +0800

    Update AsyncLockTests.cs

--- MindLab.Threading.Tests/src/AsyncLockTests.cs
+++ MindLab.Threading.Tests/src/AsyncLockTests.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 
 namespace MindLab.Threading.Tests
@@ -23,6 +26,38 @@ namespace MindLab.Threading.Tests
             var l2Task = locker.LockAsync();
             disposer.Dispose();
             Assert.IsTrue(l2Task.Wait(1000));
+        }
+
+        [TestMethod]
+        public async Task LockAsync_LockAgainWithCancel_OperationCancelled()
+        {
+            var locker = new AsyncLock();
+            using (await locker.LockAsync())
+            {
+                using var tokenSrc = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+                await Assert.ThrowsExceptionAsync<OperationCanceledException>(async () => await locker.LockAsync(tokenSrc.Token));
+            }
+        }
+
+        [TestMethod]
+        public async Task LockAsync_SafeInMultiThreads()
+        {
+            int value = 0;
+            var locker = new AsyncLock();
+
+            async Task IncreaseAsync()
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    using (await locker.LockAsync())
+                    {
+                        value++;
+                    } 
+                }
+            }
+
+            await Task.WhenAll(Enumerable.Repeat((Func<Task>) IncreaseAsync, 10).Select(func => func()).ToArray());
+            Assert.AreEqual(100, value);
         }
     }
 }
